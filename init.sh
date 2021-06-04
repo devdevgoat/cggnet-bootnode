f3938a1bbb967a7d7b42af204f8bdd0a79397d8b
Russ <russell.w.lamb@gmail.com>
2021-06-04 15:43:00 -0400

    bugs

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade
-
-apt-get install ethereum
+add-apt-repository -y ppa:ethereum/ethereum
+apt-get install -y ethereum
 mkdir data
 geth init --datadir data genesis.json
 ./start-cggnet.sh
--- start-cggnet.sh
+++ start-cggnet.sh
@@ -1,1 +1,1 @@
-geth --datadir data --networkid 201601988 --http --http.port 6437 --http.corsdomain "*"  --http.addr 24.245.71.20 --http.api personal,eth,net,web3,admin,clique
+geth --datadir data --networkid 201601988 --http --http.port 6437 --http.corsdomain "*" --http.api personal,eth,net,web3,admin,clique
