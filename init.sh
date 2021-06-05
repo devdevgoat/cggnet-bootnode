5c080a2a43b70f794f04b886d75a1c01c444426f
Russ <russell.w.lamb@gmail.com>
2021-06-04 21:24:00 -0400

    pass1

--- init.sh
+++ init.sh
@@ -3,7 +3,12 @@ apt-get update
 apt-get upgrade
 add-apt-repository -y ppa:ethereum/ethereum
 apt-get install -y ethereum
+curl https://getcroc.schollz.com | bash
+
+
 mkdir data
+$(date +%s | sha256sum | base64 | head -c 32) > password.txt
+geth --datadir=data --password ./password.txt account new > account1.txt 
 geth init --datadir data genesis.json
 
 wanip=$(dig +short myip.opendns.com @resolver1.opendns.com)
