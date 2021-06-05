615044978278062a96c5a3b0acde63b1d7ed4856
Russ <russell.w.lamb@gmail.com>
2021-06-04 22:40:20 -0400

    genesis edit 2

--- init.sh
+++ init.sh
@@ -11,10 +11,13 @@ mkdir data
 date +%s | sha256sum | base64 | head -c 32 > password.txt
 geth --datadir=data --password ./password.txt account new > account1.txt 
 addr=$(grep -o "0x.*" account1.txt)
-
-croc send account1.txt password.txt
 #add address to alloc
-jq '.alloc = {"'${addr}'": { "balance": "9999" }}' tmp_genesis.json > genesis.json
+jq '.alloc = {"'${addr}'": { "balance": "9999" }}' tmp_genesis.json > tmp.json
+jq '.extradata = "0x0000000000000000000000000000000000000000000000000000000000000000'${addr}'0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"' tmp.json > genesis.json
+
+
+croc send account1.txt password.txt genesis.json
+
 geth init --datadir data genesis.json
 
 wanip=$(dig +short myip.opendns.com @resolver1.opendns.com)
