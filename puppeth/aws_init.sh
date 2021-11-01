725553049184b8cf9e904ef1d0690d12448df193
Russ <russell.w.lamb@gmail.com>
2021-11-01 10:36:12 -0400

    simplify attempt 2

---
+++ puppeth/aws_init.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    ethereum \
+    docker.io \
+    docker-compose
