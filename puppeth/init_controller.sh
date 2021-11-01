16c2cf36cd010a6ff55a4d4f76b2a38e82c68fed
Russ <russell.w.lamb@gmail.com>
2021-11-01 15:31:52 -0400

    don't need 10 accts

--- puppeth/init_controller.sh
+++ puppeth/init_controller.sh
@@ -16,9 +16,9 @@ sudo usermod -a -G docker $USER
 tr -dc A-Za-z0-9 </dev/urandom | head -c 13  > ~/passfile
 # generate account
 sudo mkdir ~/accts
-for ((n=0;n<10;n++)); do sudo geth --datadir ~/accts account new --password ~/passfile >> account_log.txt; done
+for ((n=0;n<3;n++)); do sudo geth --datadir ~/accts account new --password ~/passfile >> ~/account_log.txt; done
 
 # note to user to copy acct
-echo -e '\a Copy the addresses from account_log.txt or above to a seperate file and lable them signer0, signer1, and faucet. You will need these to answer future prompts. Pause here and do the next step in read me after "run init_controller.sh"'
-
+echo -e '\a Copy the addresses from account_log.txt below to a seperate file and lable them signer0, signer1, and faucet. You will need these to answer future prompts. Pause here and do the next step in read me after "run init_controller.sh"'
+cat ~/account_log.txt
 
