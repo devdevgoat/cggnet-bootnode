c7a4ba1cd245fe55130a111e94947c436d4397ef
devdevgoat <russell.w.lamb@gmail.com>
2021-10-31 21:06:40 -0400

    fixed remote access
    missing http command. not putting this results in only loopback calls being allowed. https://opensourcelibs.com/lib/core-geth

--- start-master-node.sh
+++ start-master-node.sh
@@ -9,7 +9,7 @@ geth init --datadir data genesis.json
 #used for aws ip lookup
 wanip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 #runcmd='geth --datadir data --networkid 201601988 --http --http.port 6437 --http.corsdomain "*" --nodiscover --http.api personal,eth,net,web3,admin,clique --nat extip:'${wanip}' console'
-runcmd='geth --datadir data --networkid 201601988 --http --http.port 6437 --http.corsdomain "*" --nodiscover --http.api personal,eth,net,web3,admin,clique --nat extip:'${wanip}'  --nodekey bootnode.key console'
+runcmd='geth --datadir data --networkid 201601988 --http --http 0.0.0.0 --http.port 6437 --http.corsdomain "*" --nodiscover --http.api personal,eth,net,web3,admin,clique --nat extip:'${wanip}'  --nodekey bootnode.key console'
 #saved to rerun on reboot
 echo ${runcmd}>restart-master-node.sh
 chmod +x restart-master-node.sh
